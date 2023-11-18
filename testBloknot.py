b834f597098b755653bbb86e6c26a9e1044eb8e7
Zagidin <prap0235@gmail.com>
2023-11-18 17:55:02 +0300

    Леонид зверь

--- testBloknot.py
+++ testBloknot.py
@@ -1,26 +1,3 @@
-# print оператор чтоб что-то вывести
-# \n типо отступ
-# input() принятие инфы с клавы
-# это коменты #
+cycles2 = int(input('Введите любое число: '))
 
-# print("Привет! Это простой канкулятор.\nДоступные операции: + ; -")
-# a = input('Выберете операцию: ')
-
-# # создали 2 переменные с вводом чисел
-# num1 = float(input('Введите первое число: '))
-# num2 = float(input('Введите второе число: '))
-
-# if a == '+':
-#     res1 = num1 + num2
-#     print(f"Результат: {res1}")
-# elif a == '-':
-#     res2 = num1 - num2
-#     print(f"Результат: {res2}")
-# else:
-#     print("Вы ввели что-то неправильно!")
-
-# a = -1
-
-# while a<5:
-#     a += 1 
-#     print(a)
+[print(i, end=' | ') if cycles2 != i  else print(i, end=' | \n') for i in range(0, cycles2 + [1 if cycles2 > 0 else -1][0], [1 if cycles2 > 0 else -1][0])]
