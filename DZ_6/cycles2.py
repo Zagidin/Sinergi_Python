82995f589e8b7e80de6eb8c7d106c7ff7ab21f85
Zagidin <prap0235@gmail.com>
2023-11-18 03:17:47 +0300

    добавил дз 6

---
+++ DZ_6/cycles2.py
@@ -0,0 +1,18 @@
+cycles2 = int(input('Введите любое число: '))
+
+# Если число положительное 
+if cycles2 > 0:
+    contener = -1
+    while contener < cycles2:
+        contener += 1
+        print(contener, end=' | ')
+        
+# Если число отрицательное 
+elif cycles2 < 0:
+    contener = 1
+    while contener > cycles2:
+        contener -= 1
+        print(contener, end=' | ')
+
+# Это чтоб перейти на новую строку
+print()
