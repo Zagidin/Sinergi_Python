4be1440ba4deecdc164e5ab635496d598ecaaffd
Zagidin <prap0235@gmail.com>
2023-11-21 00:39:19 +0300

    DZ8

---
+++ DZ_8/dickt2.py
@@ -0,0 +1,21 @@
+# Создаём словарь с данными 
+my_dict = {
+    "name": "John",
+    "age": 25,
+    "city": "New York"
+}
+
+# Выводим наш словарь с даными
+print(my_dict, "\n")
+
+# Меняем возраст
+my_dict["age"] = 26
+
+# Добавляем ключ email со значение "john@example.com"
+my_dict["email"] = "john@example.com"
+
+# Удаляем ключ со значение, можно написать del my_dict["city"]
+my_dict.pop("city")
+
+# Вывести на экран словарь получившийся по формату примера
+print(f'Ключ: {list(my_dict.keys())[0]}, Значение: {my_dict["name"]}\nКлюч: {list(my_dict.keys())[1]}, Значение: {my_dict["age"]}\nКлюч: {list(my_dict.keys())[2]}, Значение: {my_dict["email"]}')
