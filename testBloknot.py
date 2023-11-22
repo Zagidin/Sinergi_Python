26043d6330dbb37181e154bd8ccf70758adab9b2
Zagidin <prap0235@gmail.com>
2023-11-22 03:06:07 +0300

    Zaga

--- testBloknot.py
+++ testBloknot.py
@@ -230,22 +230,22 @@ def Otvet():
     new_window = Toplevel()
     new_window.title("РКСИ")
     new_window.geometry("200x50")
-    text_new_window = Label(new_window, text="Мы не сомнивались!", pady=10)
+    text_new_window = Label(new_window, text="Мы не сомнивались!\nСиди дома и отдыхай)", pady=10)
     text_new_window.pack()
 
 
 root = Tk()
-root.geometry("165x130")
+root.geometry("165x135")
 
 root.title("ОПРОС")
 
-text = Label(root, text="Вы довольны РКСИ?", pady=20)
+text = Label(root, text="Пойти на 1 пару?", pady=20)
 text.pack()
 
-bt1 = Button(root, text="ДА", padx=10, pady=5, command=Otvet)
+bt1 = Button(root, text="ДА", font=("bold", 10), padx=10, pady=5, command=Otvet)
 bt1.pack()
 
-bt2 = Button(root, text="НЕТ", padx=10, pady=5)
+bt2 = Button(root, text="НЕТ", font=("bold", 10), padx=10, pady=5)
 bt2.bind("<Motion>", no_bt2)
 bt2.pack()
 
